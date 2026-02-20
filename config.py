7141420a73f002c602f616408e8624bf3376e638
itemgamersagar-cloud <item.gamer.sagar@gmail.com>
2026-02-20 23:25:26 +0600

    Update BOT_TOKEN and FORCE_CHANNELS in config.py
    Replaced BOT_TOKEN with a hardcoded value and updated FORCE_CHANNELS.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8479093660:AAEKuDY7svesNPrjxgO_AdabCB41MTB__Yw")
 
 ADMINS = [7503462902]  # Replace with your Telegram ID
 
@@ -10,5 +10,5 @@ DAILY_BONUS = 15
 MIN_WITHDRAW = 50
 
 FORCE_CHANNELS = [
-    "@yourchannelusername"
+    "@earn_easy_crypto_money"
 ]
