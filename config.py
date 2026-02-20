a0250a20de65b3d5e77cb430c4a2426c8a4bf7ec
itemgamersagar-cloud <item.gamer.sagar@gmail.com>
2026-02-20 23:10:02 +0600

    Add configuration settings for bot

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMINS = [7503462902]  # Replace with your Telegram ID
+
+WELCOME_BONUS = 10
+REFERRAL_BONUS = 5
+DAILY_BONUS = 15
+MIN_WITHDRAW = 50
+
+FORCE_CHANNELS = [
+    "@yourchannelusername"
+]
